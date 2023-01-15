22420a1f65a2e77d95cec02473d704d046f94b30
n1ckel <nickel990@gmail.com>
2023-01-15 14:20:53 +0300

    Homework #4

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from authapp.views import CustomUserModelViewSet
+from authapp.views import CustomUserViewSet
 from todoapp.views import ProjectModelViewSet, TodoModelViewSet
 
 router = DefaultRouter()
-router.register('users', CustomUserModelViewSet)
+router.register('users', CustomUserViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', TodoModelViewSet)
 
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,14 +1,33 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
+from .filters import ProjectFilter, ToDoFilter
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    pagination_class = TodoLimitOffsetPagination
+    filterset_class = ToDoFilter
 
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
